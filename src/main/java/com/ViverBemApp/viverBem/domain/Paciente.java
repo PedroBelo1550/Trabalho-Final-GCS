483cc8f66c45cc7592807472bad5a8c8533c3c44
DaniM-R <daniellarmelo.gmail.com>
2023-04-18 17:50:07 -0300

    Criação do campo email em paciente

--- src/main/java/com/ViverBemApp/viverBem/domain/Paciente.java
+++ src/main/java/com/ViverBemApp/viverBem/domain/Paciente.java
@@ -19,6 +19,8 @@ public class Paciente {
 
     private String data_nascimento;
 
+    private String email;
+
     public Paciente(){}
 
     @Deprecated
@@ -69,6 +71,9 @@ public class Paciente {
         this.data_nascimento = data_nascimento;
     }
 
+    public String getEmail() {  return email;  }
+
+    public void setEmail(String email) { this.email = email;  }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
