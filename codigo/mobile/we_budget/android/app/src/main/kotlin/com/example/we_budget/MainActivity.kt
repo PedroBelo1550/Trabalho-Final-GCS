314f94d9b737861412d4c8837eaa9316fffe5b36
pedro.belo <pedrohenrique1550@hotmail.com>
2023-06-14 21:35:34 -0300

    d

--- codigo/mobile/we_budget/android/app/src/main/kotlin/com/example/we_budget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.we_budget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- codigo/mobile/we_budget/test/auth_test.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:we_budget/models/auth.dart';
-
-void main() {
-}
